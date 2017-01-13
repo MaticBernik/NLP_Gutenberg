7d28ac93f1e04c14df7045265c590fec8e9b9d16
ZingBa <robert.tovornik@gmail.com>
2017-01-13 05:19:26 +0100

    Harvesting complete
    Harvester optimized, file name lenght limited to 15 char, with alphanum
    regex, book folder contains subfolders named after last name of author.
    Inner folders are txt and header, the first contains full text, the
    other metada. Name of the files are the same, with header files having
    added "_h".

--- authors_test_authent.R
+++ authors_test_authent.R
@@ -7,7 +7,7 @@ library('dplyr')
 library('gutenbergr')
 library('tidyr')
 
-authors<-c('Dickens, Charles','Carroll, Lewis', 'Twain, Mark', 'Joyce, James', 'Wilde, Oscar', 'Plato', 'Dostoyevsky, Fyodor', 'Shakespeare, William', 'Homer', 'Defoe, Daniel', 'Christie, Agatha', 'Flaubert, Gustave', 'Einstein, Albert', 'Asimov, Isaac', 'London, Jack', 'Ibsen, Henrik', 'Austen, Jane', 'Rand, Ayn', 'Tyler, Anna Cogswell', 'Shelley, Mary Wollstonecraft', 'Eliot, George', 'Chopin, Kate', 'Brontë, Emily', 'Alcott, Louisa May', 'Wharton, Edith', 'West, Rebecca', 'Woolf, Virginia', 'Cather, Willa', 'Rand, Ayn' )
+authors<-c('Dickens, Charles','Carroll, Lewis', 'Twain, Mark', 'Joyce, James', 'Wilde, Oscar', 'Plato', 'Dostoyevsky, Fyodor', 'Shakespeare, William', 'Homer', 'Defoe, Daniel', 'Christie, Agatha', 'Flaubert, Gustave', 'Einstein, Albert', 'Asimov, Isaac', 'London, Jack', 'Ibsen, Henrik', 'Austen, Jane', 'Rand, Ayn', 'Tyler, Anna Cogswell', 'Shelley, Mary Wollstonecraft', 'Eliot, George', 'Chopin, Kate', 'Alcott, Louisa May', 'Wharton, Edith', 'West, Rebecca', 'Woolf, Virginia', 'Cather, Willa', 'Rand, Ayn' )
 #go through all authors
 for (a in authors){
 	print(a)
